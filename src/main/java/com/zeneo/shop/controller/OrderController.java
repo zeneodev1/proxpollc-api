b77b62771ce9c6fc60286af398a0af7e53072160
Mohamed <zeneodev1@gmail.com>
2021-02-08 13:15:45 +0100

    first

--- src/main/java/com/zeneo/shop/controller/OrderController.java
+++ src/main/java/com/zeneo/shop/controller/OrderController.java
@@ -1,7 +1,24 @@
 package com.zeneo.shop.controller;
 
+import com.zeneo.shop.persistance.entity.Order;
+import com.zeneo.shop.persistance.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
-@Controller
+@RestController
+@RequestMapping("/orders")
 public class OrderController {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @GetMapping
+    public Flux<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
 }
--- src/main/java/com/zeneo/shop/controller/ProductController.java
+++ src/main/java/com/zeneo/shop/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.zeneo.shop.persistance.entity.Category;
 import com.zeneo.shop.persistance.entity.Department;
 import com.zeneo.shop.persistance.entity.Product;
 import com.zeneo.shop.persistance.repository.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -17,6 +18,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 
+@Slf4j
 @RestController
 @RequestMapping("/products")
 public class ProductController {
@@ -38,11 +40,15 @@ public class ProductController {
     }
     
     private void decreaseDepartment(String id) {
-        mongoTemplate.updateFirst(Query.query(Criteria.where("id").is(id)), new Update().inc("productCount", -1), Department.class).subscribe();
+        mongoTemplate
+                .updateFirst(Query.query(Criteria.where("id").is(id)), new Update().inc("productCount", -1), Department.class)
+                .subscribe();
     }
 
     private void decreaseCategory(String id) {
-        mongoTemplate.updateFirst(Query.query(Criteria.where("id").is(id)), new Update().inc("productCount", -1), Category.class).subscribe();
+        mongoTemplate
+                .updateFirst(Query.query(Criteria.where("id").is(id)), new Update().inc("productCount", -1), Category.class)
+                .subscribe();
     }
 
     @GetMapping("/{id}")
@@ -74,12 +80,13 @@ public class ProductController {
     public Mono<Product> addProduct(@RequestBody Product product) {
         return productRepository
                 .save(product)
+                .log("save product")
                 .doOnNext((p) -> {
                     increaseCategory(p.getCategoryId());
-                })
+                }).log("increase category")
                 .doOnNext(p -> {
                     increaseDepartment(p.getDepartmentId());
-                });
+                }).log("increase department");
     }
 
     @PutMapping
@@ -89,15 +96,16 @@ public class ProductController {
     }
 
     @DeleteMapping("/{id}")
-    public Mono<Object> deleteProduct(@PathVariable String id) {
-        return productRepository.findById(id).map(product ->
-                productRepository.deleteById(id)
-                .doOnNext((v) -> {
-                    decreaseCategory(product.getCategoryId());
-                }).doOnNext((v) -> {
+    public Mono<Product> deleteProduct(@PathVariable String id) {
+        return productRepository
+                .findById(id)
+                .doOnNext(product -> {
+                    productRepository.delete(product).subscribe();
+                }).doOnNext(product -> {
                     decreaseDepartment(product.getDepartmentId());
-                })
-        );
+                }).doOnNext(product -> {
+                    decreaseCategory(product.getCategoryId());
+                });
 
     }
 
