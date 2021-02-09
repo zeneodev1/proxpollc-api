a89df3b08672e9a073b3d8c8d0948f35d73fafe3
Mohamed <zeneodev1@gmail.com>
2021-02-09 13:23:47 +0100

    fix changePassword

--- src/main/java/com/zeneo/shop/controller/UserController.java
+++ src/main/java/com/zeneo/shop/controller/UserController.java
@@ -12,10 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -39,7 +36,8 @@ public class UserController {
 
     @PostMapping(value = "/login", produces = {MediaType.TEXT_PLAIN_VALUE})
     private Mono<ResponseEntity<?>> login (@RequestBody LoginRequest loginRequest) {
-        return Mono.just(loginRequest).flatMap((loginRequest1) -> usersService.findByUsername(loginRequest.getEmail())
+        return Mono.just(loginRequest)
+                .flatMap((loginRequest1) -> usersService.findByUsername(loginRequest.getEmail())
                 .map((userDetails -> passwordEncoder.matches(loginRequest.getPassword(), userDetails.getPassword())
                         ? ResponseEntity.ok(jwtUtil.generateToken(userDetails))
                         : UNAUTHORIZED)))
@@ -47,7 +45,7 @@ public class UserController {
     }
 
 
-    @PostMapping(value = "/changePassword")
+    @PutMapping(value = "/changePassword")
     private Mono<ResponseEntity<Object>> changePassword (@RequestBody ChangePassword changePassword) {
         return Mono.just(changePassword)
                 .flatMap((loginRequest1) -> usersService.findByUsername(changePassword.getEmail())
